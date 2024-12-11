01a1386f142de76f1e55e40fb9918d7ec109a78a
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2024-12-11 09:11:00 +0700

    Feat: add function to create new task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,24 @@ import TaskCard from './components/TaskCard';
 
 function App() {
   const [tasks, setTasks] = useState();
+  const [inputTask, setInputTask] = useState({
+    "name": "",
+    "is_checked": false,
+    "created_at": new Date()
+  });
+
   const fetchTasks = async () => {
     try {
       const { data } = await axios.get('http://localhost:3000/todos?_sort=-created_at');
       setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const addTask = async () => {
+    try {
+      await axios.post("http://localhost:3000/todos", inputTask);
     } catch (error) {
       console.log(error);
     }
@@ -24,8 +38,8 @@ function App() {
         <div className='w-full flex flex-col gap-2 items-center mt-8'>
 
           {/* form input task */}
-          <form className='w-64 flex gap-2 mb-6'>
-            <input className='py-2 px-4 w-full border-2 border-black focus:outline-none focus:bg-[#eee] peer' />
+          <form className='w-64 flex gap-2 mb-6' onSubmit={() => addTask()}>
+            <input onChange={(e) => setInputTask({ ...inputTask, name: e.target.value })} className='py-2 px-4 w-full border-2 border-black focus:outline-none focus:bg-[#eee] peer' />
             <button className='py-2 px-4 border-2 border-black hover:bg-black/50 hover:text-white active:bg-white active:text-black peer-focus:bg-black peer-focus:hover:bg-black/50 peer-focus:text-white'>+</button>
           </form>
           {/* end form input task */}
